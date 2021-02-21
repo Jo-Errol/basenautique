438538c85a334a49259c9d29e6098e1b710a092e
jbonnassieux <jbonnassieux@sqli.com>
2021-02-21 23:09:02 +0100

    architecture, page principale, images

--- basenautique/js/script.js
+++ basenautique/js/script.js
@@ -1,27 +1,38 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /*loop through a collection of all HTML elements:*/
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute("includes");
+    var page, elem, file, xhttp;
+    page = document.getElementsByTagName("*");
+    for (var i = 0; i < page.length; i++) {
+        elem = page[i];
+        file = elem.getAttribute("includes");
         if (file) {
-            /*make an HTTP request using the attribute value as the file name:*/
             xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4) {
-                    if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-                    if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-                    /*remove the attribute, and call this function once more:*/
-                    elmnt.removeAttribute("includes");
+                    if (this.status == 200) {elem.innerHTML = this.responseText;}
+                    if (this.status == 404) {elem.innerHTML = "Page not found.";}
+                    elem.removeAttribute("includes");
                     includeHTML();
                 }
             }
             xhttp.open("GET", file, true);
             xhttp.send();
-            /*exit the function:*/
             return;
         }
+    }
+};
+
+$(function() {
+    $("#calendar").datepicker({
+        altField: "#calendardate"
+    });
+});
+
+function isChecked(checkbox, valid) {
+    var button = document.getElementById(valid);
+
+    if (checkbox.checked === true) {
+        button.disabled = "";
+    } else {
+        button.disabled = "disabled";
     }
 };
